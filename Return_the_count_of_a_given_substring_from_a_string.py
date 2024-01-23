c6e5c8d314126f7ba8be0ccfd4bfb6000f591efc
unknown <centinaraphael0157@gmail.com>
2024-01-23 23:05:01 +0800

    Working code

---
+++ Return_the_count_of_a_given_substring_from_a_string.py
@@ -0,0 +1,5 @@
+#Write a program to find how many times substring “Emma” appears in the given string.
+text_given = str('Emma is good developer. Emma is a writer')
+
+Emma_count = text_given.count('Emma')
+print ('The word "Emma" appeared ', Emma_count, 'times')
